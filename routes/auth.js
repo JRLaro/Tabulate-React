2981c233c0d6c48537842cdffbb5c0b80933ccf0
J.Laro <joselarojr@gmail.com>
2021-04-15 11:11:15 -0400

    created routes

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+//@route --> GET api/auth
+//@description --> Get logged in user 
+//@access --> Private 
+router.get('/', (req, res) => {
+    res.send('Get a User')
+});
+
+//@route --> POST api/auth
+//@description --> Auth user and get token 
+//@access --> Public 
+router.post('/', (req, res) => {
+    res.send('Log in User')
+});
+
+module.exports = router;
