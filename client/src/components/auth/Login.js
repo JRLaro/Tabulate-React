2e7b5e343d43319b4fab58dc8b14cfb6c24e249b
J.Laro <joselarojr@gmail.com>
2021-04-21 10:32:07 -0400

    user login/load user

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,39 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
+import AlertContext from "../../context/alert/alertContext";
 
-function Login() {
+function Login(props) {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+    if (error === "Invalid email") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
   const { email, password } = user;
 
-    const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
-    
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log('Login Successful');
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      setAlert('Invalid emailPlease fill in all fields', 'danger');
+    } else {
+      login({
+        email,
+        password
+      })
     }
+  };
 
   return (
     <div className="reg-container shadow p-3 mb-5 mt-5 bg-white rounded">
       <h1 className="text-center">
         Account <span className="text-danger">Login</span>
       </h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
-          <label htmlFor="email"> <i class="fas fa-envelope"></i> Email</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+          <label htmlFor="email">
+            {" "}
+            <i class="fas fa-envelope"></i> Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="password"> <i class="fas fa-lock"></i> Password</label>
+          <label htmlFor="password">
+            {" "}
+            <i class="fas fa-lock"></i> Password
+          </label>
           <input
             type="password"
             name="password"
             value={password}
             onChange={onChange}
+            required
           />
         </div>
-              <input type="submit" value='Login' className="btn btn-primary btn-block" />
+        <input
+          type="submit"
+          value="Login"
+          className="btn btn-primary btn-block"
+        />
       </form>
     </div>
   );
