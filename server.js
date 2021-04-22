609807264e1b3fc68a6a845a2e2ab1b051f0dd5c
J.Laro <joselarojr@gmail.com>
2021-04-22 10:16:53 -0400

    Prep for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
 // initialize express into 'app'
 const app = express();
 
@@ -17,6 +18,19 @@ app.get("/", (req, res) => res.json({ msg: "Tabulate" }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/notes", require("./routes/notes"));
 app.use("/api/auth", require("./routes/auth"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get(
+    "*",
+    (req,
+    (res) =>
+      res.sendFile(path.resolve(_dirname, "client", "build", "index.html")))
+  );
+}
 
 //Port
 const PORT = process.env.PORT || 5000;
